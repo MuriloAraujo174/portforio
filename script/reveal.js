eb0ff712a62e3536fbd0b61b5da7b31649cca55a
antonio murilo <107326926+MuriloAraujo174@users.noreply.github.com>
2023-12-21 14:32:00 -0300

    Add files via upload

---
+++ script/reveal.js
@@ -0,0 +1,14 @@
+window.revelar = ScrollReveal({reset: true});
+
+revelar.reveal('.efeito-txt-topo',{
+    duration: 2000,
+    dalay: 500,
+    distance: '90px',
+
+})
+
+revelar.reveal('.efeito-img-topo',{
+    duration: 2000,
+    dalay: 500,
+    distance: '90px',
+})
